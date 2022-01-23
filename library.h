d05e9bbd2d6218372f81eeb3c4cbc45b57866763
Geoffrey Lin <glin20@stuy.edu>
2022-01-23 14:46:14 -0500

    readded saving/opening?

--- library.h
+++ library.h
@@ -8,3 +8,5 @@ struct library{
 struct library *make_library();
 struct library *add_to_library(struct library *data, char *filename);
 struct library *remove_from_library(struct library *data, char *name);
+void save_library(struct library *data);
+struct library *read_library();
